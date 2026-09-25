dc7fb9ed6111c1caa92ee85d76210d15f0a653d4
Pichau <gabrielcake9@gmail.com>
2026-09-24 21:49:09 -0300

    adiciona imagens e componente de captura de tela

---
+++ src/components/ScreenCapture.js
@@ -0,0 +1,5 @@
+export default function ScreenCapture({ src }) {
+  return (
+    <img src={src} style={{maxWidth: '80%'}}/>
+  );
+}
--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -2,6 +2,7 @@ import React from 'react';
 // Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
 import YouTubeVideo from '../components/YoutubeVideo';
+import ScreenCapture from '../components/ScreenCapture';
 
 export default {
   // Re-use the default mapping
@@ -9,4 +10,5 @@ export default {
   // Map the "<Highlight>" tag to our Highlight component
   // `Highlight` will receive all props that were passed to `<Highlight>` in MDX
   YouTubeVideo,
+  ScreenCapture,
 };
